7543d75ba0f403ef088b7dcef6cb21b1c98b3844
xiaoTaoist <taoist@protonmail.com>
2018-07-03 14:15:43 +0800

    update freess.py

--- freess.py
+++ freess.py
@@ -11,115 +11,122 @@ from PIL import Image
 from Cryptodome.Cipher import AES
 from Cryptodome.Util import Counter
 
+headers = {
+    'User-Agent':
+    'Mozilla/5.0 (Windows NT 10.0; rv:61.0) Gecko/20100101 Firefox/61.0'
+}
+
 
 def Get_Endata():
     """从URL中获取纯文本信息"""
-    
-    headers = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; rv:59.0) Gecko/20100101 Firefox/59.0'}
-    url= "http://free-ss.tk/" 
-    s = requests.session()
+    print("信息获取ing。。。。。。")
+    url = "http://www.free-ss.tk/"
+    n = 0
     try:
-        print("正在获取信息。。。。。。")
-        html = s.get(url,headers=headers,timeout=5).text
-        
-        a,b = re.findall(r"\'(.*?)\'",html)[9:11]
-        img_c  = re.findall(r"data:image/png;base64,(.*)'",html)[0]
-        img_c = BytesIO(base64.b64decode(img_c))
-        img = Image.open(img_c)
-        c = zbar.decode(img)[0].data.decode()
-        
-        p_data = {'a':a,'b':b,'c':c}
-        enmsg = s.post(url+"data.php",headers=headers,data=p_data,timeout=3)
-        endata = base64.b64decode(enmsg.text)
-        
-        key = bytes(a,encoding="utf-8")
-        iv = bytes(b,encoding="utf-8")
-        mode  = re.findall(r"CryptoJS.mode.(\w{3})",html)
-        print("获取信息成功。。。。。。。")
-        return endata,key,iv,mode
-    except:
-        print("获取失败。。。。。。")
+        while n < 5:
+            n += 1
+
+            s = requests.session()
+            html = requests.get(url, headers=headers, timeout=2).text
+
+            a = re.findall("var a='(.*?)'", html)[1]
+            b = re.findall("var b='(.*?)'", html)[1]
+            img_c = re.findall(r"data:image/png;base64,(.*)'", html)[0]
+            img_c = BytesIO(base64.b64decode(img_c))
+            img = Image.open(img_c)
+            c = zbar.decode(img)[0].data.decode()
+
+            p_data = {'a': a, 'b': b, 'c': c}
+            enmsg = requests.post(
+                url + "data.php", headers=headers, data=p_data, timeout=1)
+            endata = base64.b64decode(enmsg.content)
+
+            key = bytes(a, encoding="utf-8")
+            iv = bytes(b, encoding="utf-8")
+
+            if endata:
+                return endata, key, iv
+            print("。。。")
+        print("请求失败，请稍后重试！")
+    except Exception as e:
+        print(e)
 
 
-def Dec_data(mode,key,iv,endata):
-    try:
-        #print(mode,key,iv)
-        # 初始化AES解密方式
-        if mode == [] or mode[0] == 'CBC':
-            aes = AES.new(key,AES.MODE_CBC,iv)
-        elif mode[0] == 'ECB':
-            aes = AES.new(key,AES.MODE_ECB)
-        elif mode[0] == 'CFB':
-            aes = AES.new(key,AES.MODE_CFB,iv,segment_size=128)
-        elif mode[0] == 'OFB':
-            aes = AES.new(key,AES.MODE_OFB,iv)
-        elif mode[0] == 'CTR':
-            ctr = Counter.new(128, initial_value=int(binascii.hexlify(iv), 16))
-            aes = AES.new(key,AES.MODE_CTR,counter=ctr)
-        else:
-            print('获取加密方式错误。。。。')
+def Dec_data(key, iv, endata):
 
-        ss = re.findall(r'{.*}',aes.decrypt(endata).decode('utf-8'))[0]
-        ss_data = json.loads(ss)['data']
-        print("解密成功")
-        return ss_data
-    except:
-        print("解密失败！")
+    ctr = Counter.new(128, initial_value=int(binascii.hexlify(iv), 16))
+    modes = [
+        AES.new(key, AES.MODE_ECB),
+        AES.new(key, AES.MODE_CTR, counter=ctr),
+        AES.new(key, AES.MODE_OFB, iv),
+        AES.new(key, AES.MODE_CFB, iv, segment_size=128),
+        AES.new(key, AES.MODE_CBC, iv)
+    ]
+
+    print("解密。。。")
+    for mode in modes:
+        try:
+            aes = mode
+            dec = aes.decrypt(endata).decode('utf-8')
+            ss = re.findall(r'{.*}', dec)
+            print(mode)
+            ss_data = json.loads(ss[0])['data']
+            return ss_data
+        except Exception as e:
+            pass
+    print("解密失败！")
 
 
 def Read_Config(filename="gui-config.json"):
     try:
-        with open(filename,'r',encoding='utf-8') as f_obj:
+        with open(filename, 'r', encoding='utf-8') as f_obj:
             guiconfig = json.load(f_obj)
         print("读取配置文件成功。。。")
         return guiconfig
     except FileNotFoundError:
         msg = "文件 " + filename + " 并不存在，请确认程序运行在Shadowsocks目录中！"
         print(msg)
-    
 
 
-def Write_Config(guiconfig,ss_data,filename="gui-config.json"):
+def Write_Config(guiconfig, ss_data, filename="gui-config.json"):
     configs = []
 
     counts = 0
-   
     for ss in ss_data:
         config = {}
-
         #移除低分
         try:
-            if sum(map(int,ss[0].split('/'))) < 34:
+            if sum(map(int, ss[0].split('/'))) < 34:
                 continue
         except:
             pass
 
-        #限制获取数量为10
+        if (re.match(r"aes|rc4|chacha", ss[3])):
+            ss[3], ss[4] = ss[4], ss[3]
+
+        #限制获取数量为8
         counts += 1
-        if(counts > 10):
+        if (counts > 8):
             break
 
-        if (re.match(r"aes|rc4|chacha",ss[3])):
-            ss[3],ss[4] = ss[4],ss[3] 
-        
         config["password"] = ss[3]
-        config["method"] =  ss[4]
+        config["method"] = ss[4]
 
         config["server"] = ss[1]
         config["server_port"] = ss[2]
-        
-        config["plugin"] =  ""
-        config["plugin_opts"] =  ""
-        config["remarks"] = "{}[{}]({})".format(ss[6],ss[0],ss[5])
+
+        config["plugin"] = ""
+        config["plugin_opts"] = ""
+        config["remarks"] = "{}[{}]({})".format(ss[6], ss[0], ss[5])
         config["timeout"] = 2
         configs.append(config)
     guiconfig['configs'] = configs
 
     print('正在写入文件……')
-    print('此次更新了'+str(len(configs))+'条数据')
+    print('此次更新了' + str(len(configs)) + '条数据')
     try:
-        with open(filename,'w',encoding='utf-8') as f_obj:
-            f_obj.write(json.dumps(guiconfig,ensure_ascii=False,indent=2))
+        with open(filename, 'w', encoding='utf-8') as f_obj:
+            f_obj.write(json.dumps(guiconfig, ensure_ascii=False, indent=2))
         print('写入文件完成！')
     except FileNotFoundError:
         msg = "文件 " + filename + " 并不存在，请确认程序运行在Shadowsocks目录中！"
@@ -127,23 +134,23 @@ def Write_Config(guiconfig,ss_data,filename="gui-config.json"):
 
 
 def Kill_SS():
-        try:
-            os.system("taskkill /F /IM Shadowsocks.exe /T")
-        except:
-            pass
+    try:
+        os.system("taskkill /F /IM Shadowsocks.exe /T")
+    except:
+        pass
+
 
 def Start_SS():
     os.startfile("Shadowsocks.exe")
 
 
 if __name__ == '__main__':
-
-    endata,key,iv,mode = Get_Endata()
-    ss_data = Dec_data(mode,key,iv,endata)
+    endata, key, iv = Get_Endata()
+    ss_data = Dec_data(key, iv, endata)
     guiconfig = Read_Config()
-    
+
     Kill_SS()
-    Write_Config(guiconfig,ss_data)
+    Write_Config(guiconfig, ss_data)
     Start_SS()
 
     print("3s后关闭......")
